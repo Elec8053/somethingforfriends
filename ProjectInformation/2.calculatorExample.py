31ddc53b3c1b3b515cace821be913457d3f3bec5
Elec8053 <132301997+Elec8053@users.noreply.github.com>
2023-05-01 22:04:59 +0200

    Create 2.calculatorExample.py

---
+++ ProjectInformation/2.calculatorExample.py
@@ -0,0 +1,19 @@
+num1_str = input("Enter the first number: ")
+num2_str = input("Enter the second number: ")
+
+num1 = float(num1_str)
+num2 = float(num2_str)
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+remainder = num1 % num2
+power = num1 ** num2
+
+print("The sum is:", sum)
+print("The difference is:", difference)
+print("The product is:", product)
+print("The quotient is:", quotient)
+print("The remainder is:", remainder)
+print("The first number raised to the power of the second number is:", power)
